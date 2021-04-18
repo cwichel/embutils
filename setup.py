9d1a0defa5d1ff81e79ff82e77e87edd61553236
Christian Wiche <cwichel@gmail.com>
2021-04-18 03:48:05 -0400

    Updated CI for python >= 3.7

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
