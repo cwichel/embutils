2fa6d393faaed3ea59fccf307949ca06d7673925
Christian Wiche <cwichel@gmail.com>
2021-06-28 01:21:24 -0400

    Updated example documentation

--- examples/serial_scanner.py
+++ examples/serial_scanner.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 # -*- coding: ascii -*-
 """
-Serial scanner example/testing file.
+Serial scanner usage example.
 
-NOTE: Since the scanner requires actual devices being connected is not named for
-being executed on pytest.
-
-@date:      2021
-@author:    Christian Wiche
-@contact:   cwichel@gmail.com
-@license:   The MIT License (MIT)
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
 """
 
 from embutils.serial.core import SerialDeviceList, SerialDeviceScanner
@@ -18,25 +15,29 @@ import time
 
 # Example Definitions ===========================
 def on_change_handler(event: SerialDeviceScanner.Event, dev_diff: SerialDeviceList) -> None:
-    """Process the serial device change event.
-
-    Args:
-        event (SerialDeviceEvent): Serial scanner event.
-        dev_diff (SerialDeviceList): Change list.
     """
-    dev = '\r\n\t'.join('{}'.format(item) for item in dev_diff)
-    msg = '{}\r\n\t{}'.format(event.__repr__(), dev)
+    Process the serial device change event.
+
+    :param SerialDeviceScanner.Event event: Serial device scanner Event.
+    :param SerialDeviceList dev_diff: Event change list.
+    """
+    dev = '\r\n\t'.join(f'{item}' for item in dev_diff)
+    msg = f'{event.__repr__()}\r\n\t{dev}'
     print(msg)
 
 
-def test_serial_scanner() -> None:
-    """Example execution.
+def ex_serial_scanner() -> None:
+    """
+    Serial scanner example.
+
     In this example we:
-        1. Create a serial device scanner.
-        2. Set a callback function to print changes on the connected serial devices.
+
+    #. Create a serial device scanner.
+    #. Set a callback function to print changes on the connected serial devices.
+
     """
     # Create a serial scanner instance
-    ss = SerialDeviceScanner(scan_period=0.05)
+    ss = SerialDeviceScanner(period=0.05)
     ss.on_list_change += on_change_handler
 
     # Maintain this alive
@@ -46,4 +47,4 @@ def test_serial_scanner() -> None:
 
 # Example Execution =============================
 if __name__ == '__main__':
-    test_serial_scanner()
+    ex_serial_scanner()
