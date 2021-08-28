d80785d1204679634e42de9cdcea22c406e712f0
Christian Wiche <cwichel@gmail.com>
2021-08-28 16:37:40 -0400

    Refactored thread pool to actually work as one

--- embutils/utils/threading.py
+++ embutils/utils/threading.py
@@ -9,159 +9,222 @@ SDK threading utilities.
 :license:   The MIT License (MIT)
 """
 
+from abc import ABC, abstractmethod
 from functools import wraps
-from threading import Lock, Thread
-from typing import List, Union
+from queue import Empty, Queue
+from threading import RLock, Thread
+from typing import Callable, List, TypeVar
+
+from .logger import SDK_LOG
 
 
 # -->> Definitions <<------------------
+RT = TypeVar('RT')
 
 
 # -->> API <<--------------------------
-def synchronous(lock_name: str) -> callable:
+def sync(lock_name: str) -> Callable[[Callable[..., RT]], Callable[..., RT]]:
     """
-    Decorator that can be used in a class to wrap a function or action with a
-    lock.
+    Decorator used to wrap a class method with a lock defined as
+    instance/class attribute.
 
     :param str lock_name: Name of the lock attribute.
 
-    :returns: Callable with the lock wrapped function.
-    :rtype: callable
+    :returns: Locked function.
+    :rtype: Callable[[Callable[..., RT]], Callable[..., RT]]
     """
-    def _wrapper(func: callable) -> callable:
+    def wrapper(func: Callable[..., RT]) -> Callable[..., RT]:
         """
         Wraps the decorated function to apply the lock.
         """
         @wraps(func)
-        def _synchronizer(self, *args, **kwargs):
+        def synchronizer(self, *args, **kwargs) -> RT:
             """
-            Applies the lock over the given function.
+            Applies a lock over the given function.
             """
-            _lock = self.__getattribute__(lock_name)
-            with _lock:
+            lock = self.__getattribute__(lock_name)
+            with lock:
                 return func(self, *args, **kwargs)
-        return _synchronizer
-    return _wrapper
+        return synchronizer
+    return wrapper
+
+
+class _ThreadWorker(Thread):
+    """
+    Thread pool worker.
+    This represents a single thread on the pool. The thread is set as daemon or not based on
+    the pool configurations.
+    """
+    def __init__(self, name: str, queue: Queue, timeout: float) -> None:
+        """
+        Class initialization.
+
+        :param str name:        Worker name.
+        :param Queue queue:     Queue to get the tasks from.
+        :param float timeout:   Timeout for waiting for a task.
+        """
+        super(_ThreadWorker, self).__init__(name=name)
+        SDK_LOG.debug(f"Creating worker thread {self.name}")
+        self._active = True
+        self._queue = queue
+        self._timeout = timeout
+
+    def stop(self) -> None:
+        """
+        Stops the thread.
+        """
+        self._active = False
+
+    def run(self) -> None:
+        """
+        Runs the worker logic.
+
+        - Blocks until retrieves a task from the tasks queue (locked).
+        - Checks that the task is a callable and execute.
+
+        """
+        # Worker execution
+        while self._active:
+            try:
+                task = self._queue.get(timeout=self._timeout)
+                if isinstance(task, AbstractThreadTask):
+                    try:
+                        task.execute()
+                    except Exception as ex:
+                        SDK_LOG.info(f"Caught exception while running task:\n"
+                                     f"> Task   : {task}\n"
+                                     f"> Raised : {ex.__class__.__name__} {ex}")
+                    finally:
+                        self._queue.task_done()
+            except Empty:
+                pass
+
+        # Worker termination
+        SDK_LOG.debug(f"Terminating worker thread {self.name}")
+
+
+class AbstractThreadTask(ABC):
+    """
+    Thread task abstraction.
+    Use this class to define how to execute a task inside the ThreadPool.
+    """
+    @abstractmethod
+    def execute(self) -> None:
+        """
+        Execution called by the ThreadWorkers on the ThreadPool implementation.
+        """
+        pass
+
+
+class SimpleThreadTask(AbstractThreadTask):
+    """
+    Simple thread task.
+    Accepts a function to be executed by a worker on the ThreadPool.
+    """
+    def __init__(self, task: Callable[..., None], *args, **kwargs) -> None:
+        """
+        Task configuration:
+
+        :param Callable[..., None] task:    Task function.
+        :param args:                        Task arguments.
+        :param kwargs:                      Task keyword arguments.
+        """
+        # Check input
+        if not callable(task):
+            raise ValueError("The task is not a valid function")
+
+        # Store attributes
+        self._task   = task
+        self._args   = args
+        self._kwargs = kwargs
+
+    def execute(self) -> None:
+        """
+        Execution called by the ThreadWorkers on the ThreadPool implementation.
+        """
+        SDK_LOG.debug(f"Executing task {self._task.__name__}")
+        self._task(*self._args, **self._kwargs)
 
 
 class ThreadPool:
     """
-    Thread pool implementation for ThreadItem instances.
-
-    .. note::
-        * This class is implemented as a singleton.
+    Simple thread pool implementation.
+    Use queues to coordinate tasks among a set of worker threads.
     """
-
-    __inst: Union[None, 'ThreadPool'] = None
-    __init: bool = False
-
-    def __new__(cls) -> 'ThreadPool':
+    def __init__(self, size: int, name: str, timeout: float = 60, daemon: bool = True) -> None:
         """
-        Creates the singleton instance.
-
-        :returns: singleton.
-        :rtype: ThreadPool
+        :param int size:        Size of the thread pool.
+        :param str name:        Thread pool name. Used as prefix on the workers as <name>_<worker_id>.
+        :param float timeout:   Polling time used by the workers while waiting on a get() request
+                                on the task queue. This value mainly affects the time used by the
+                                threads to terminate when terminate() is called.
+        :param bool daemon:     Set to true if the threads should immediately terminate when the
+                                main thread exists.
         """
-        if cls.__inst is None:
-            cls.__inst = super(ThreadPool, cls).__new__(cls)
-            cls.__init = False
-        return cls.__inst
+        # Input checking
+        if size < 1:
+            raise ValueError("Thread pool should have at least one worker")
+        if timeout <= 0:
+            raise ValueError("Polling timeout should be greater than zero")
 
-    def __init__(self) -> None:
-        """
-        This class don't require any input from the user to be initialized.
-        """
-        # Only do this once
-        if self.__init:
-            return
-        self.__init = True
+        # Set attributes
+        self._size = size
+        self._daemon = daemon
+        self._prefix = name
+        self._timeout = timeout
+        self._rlock = RLock()
+        self._queue = Queue()
+        self._workers: List[_ThreadWorker] = []
 
-        # Initialize
-        self._lock:     Lock = Lock()
-        self._threads:  List[Thread] = []
+        # Create workers
+        self._create_workers()
 
     @property
-    @synchronous(lock_name='_lock')
-    def count(self) -> int:
+    @sync(lock_name='_rlock')
+    def size(self) -> int:
         """
-        Number of threads registered in the pool.
-        """
-        return len(self._threads)
+        Returns the size of the thread pool.
 
-    @property
-    @synchronous(lock_name='_lock')
-    def active(self) -> int:
+        :returns: Number of workers.
+        :rtype: int
         """
-        Number of active threads in the pool.
-        """
-        return sum([1 if t.is_alive() else 0 for t in self._threads])
+        return self._size
 
-    @synchronous(lock_name='_lock')
-    def add(self, thread: Thread) -> None:
+    def enqueue(self, task: AbstractThreadTask) -> None:
         """
-        Add a thread to the pool.
+        Enqueue a task on the thread pool to be executed by the workers.
 
-        :param Thread thread: Thread to be added into the pool.
+        :param AbstractThreadTask task: Task to added to the queue.
         """
-        self._threads.append(thread)
+        # Check input
+        if not isinstance(task, AbstractThreadTask):
+            raise ValueError(f"The task need to be {AbstractThreadTask.__name__} or a subclass of it")
+        # Enqueue
+        self._queue.put(task)
 
-    @synchronous(lock_name='_lock')
-    def remove(self, thread: Thread) -> None:
+    @sync(lock_name='_rlock')
+    def stop(self) -> None:
         """
-        Remove a thread from the pool (if available).
+        Wait for the task queue to be completed and stops all the workers.
+        """
+        SDK_LOG.debug("Waiting for all the tasks to be handled...")
+        self._queue.join()
+        SDK_LOG.debug("Terminating worker threads...")
+        for worker in self._workers:
+            worker.stop()
 
-        :param Thread thread: Thread to be removed from the pool.
+    @sync(lock_name='_rlock')
+    def _create_workers(self) -> None:
         """
-        if thread in self._threads:
-            idx = self._threads.index(thread)
-            self._threads.pop(idx)
+        Create all the pool workers.
+        """
+        while len(self._workers) < self._size:
+            worker = _ThreadWorker(name=f"{self._prefix}_{(len(self._workers) + 1)}", queue=self._queue, timeout=self._timeout)
+            worker.setDaemon(self._daemon)
+            worker.start()
+            self._workers.append(worker)
 
 
-class ThreadItem(Thread):
-    """
-    Thread item implementation. All the threads created using this class (daemons
-    or not) can be monitored using the ThreadPool class.
-    """
-    def __init__(self,
-                 *args,
-                 name: str, target: callable,
-                 daemon: bool = True, auto_remove: bool = True,
-                 **kwargs) -> None:
-        """
-        Class initialization.
-
-        :param str name:            Thread name. Used to identify the thread on the pool.
-        :param bool daemon:         If true the thread will be initialized as daemon.
-        :param bool auto_remove:    If true the thread will be deleted after completion.
-        """
-        # Create thread
-        super(ThreadItem, self).__init__(name=name, target=target, *args, **kwargs)
-        self._auto_remove = auto_remove
-        self.setDaemon(daemon)
-
-        # Add thread to pool and start
-        ThreadPool().add(thread=self)
-        self.start()
-
-    def __del__(self) -> None:
-        """
-        Class destructor. This method ensures that the thread is removed from the
-        pool when deleted.
-        """
-        ThreadPool().remove(thread=self)
-
-    def run(self) -> None:
-        """
-        Runs the thread target process. If auto remove is set to true this method
-        will attempt to delete the thread when the process finishes the execution.
-        """
-        super().run()
-        if self._auto_remove:
-            self.remove()
-
-    def remove(self):
-        """
-        Removes the thread item from the pool and attempts to delete itself.
-        """
-        ThreadPool().remove(thread=self)
-        del self
+# -->> Instances <<--------------------
+#: Embutils internal thread pool
+SDK_TP = ThreadPool(size=25, name='EMBUTILS_Thread_', timeout=30)
