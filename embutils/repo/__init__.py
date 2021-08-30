47e5472441a8cbe8d0063dcba3519344ca38ae95
Christian Wiche <cwichel@gmail.com>
2021-08-30 00:44:47 -0400

    Fixed typo on repo utilities import

--- embutils/repo/__init__.py
+++ embutils/repo/__init__.py
@@ -1,3 +1,3 @@
-from .binary import bin_to_hex, merge_hex, merge_hex
+from .binary import bin_to_hex, merge_bin, merge_hex
 from .build import build_cubeide, build_iar
 from .version import Version, VersionGit, VersionSVN
