db22e832ae064d1d6b796109f1b77c02c0dd45aa
Christian Wiche <cwichel@gmail.com>
2021-11-04 12:23:39 -0300

    Separated and simplified stream redirect

--- embutils/utils/subprocess.py
+++ embutils/utils/subprocess.py
@@ -9,87 +9,18 @@ Subprocess execution utilities.
 :license:   The MIT License (MIT)
 """
 
-import queue
 import subprocess as sp
 import sys
 import time
-import typing as tp
 
 from .path import TPPath, path_validator
-from .threading import SDK_TP, ThreadPool, SimpleThreadTask
+from .stream import StreamRedirect
 
 
 # -->> Definitions <<------------------
 
 
 # -->> API <<--------------------------
-class StreamRedirect:
-    """
-    Stream redirect utility implementation.
-    Allows to store and redirect a given stream.
-    """
-    def __init__(self,
-                 name: str, stream_in: tp.IO[tp.AnyStr], stream_out: tp.IO[tp.AnyStr],
-                 pool: ThreadPool = SDK_TP
-                 ) -> None:
-        """
-        Class initialization.
-
-        :param str name:                Stream redirection name (used for naming threads).
-        :param IO[AnyStr] stream_in:    Input stream. Will be stored on buffer and redirected to output.
-        :param IO[AnyStr] stream_out:   Output stream.
-        :param ThreadPool pool:         Thread pool to be used by the stream redirection.
-        """
-        self._name  = name
-        self._src   = stream_in
-        self._out   = stream_out
-        self._pool  = pool
-        self._buff  = []
-        self._queue = queue.Queue()
-        self._ready = False
-        self._start()
-
-    @property
-    def buffer(self) -> str:
-        """
-        Stream buffer.
-        """
-        return "".join(self._buff)
-
-    def wait_ready(self) -> None:
-        """
-        Waits until the stream is finished to continue.
-        """
-        while not self._ready:
-            time.sleep(0.01)
-
-    def _start(self) -> None:
-        """
-        Initializes the redirection read/write threads.
-        """
-        self._pool.enqueue(SimpleThreadTask(name=f"{self.__class__.__name__}_{self._name}_read", task=self._read))
-        self._pool.enqueue(SimpleThreadTask(name=f"{self.__class__.__name__}_{self._name}_write", task=self._write))
-
-    def _write(self) -> None:
-        """
-        Write every line into the output stream and buffer.
-        """
-        for line in iter(self._queue.get, None):
-            self._out.write(line)
-
-    def _read(self) -> None:
-        """
-        Parses and copies every line of the input stream.
-        """
-        for line in iter(self._src.readline, b""):
-            line = line.decode()
-            self._buff.append(line)
-            self._queue.put(line)
-        self._src.close()
-        self._queue.put(None)
-        self._ready = True
-
-
 def execute(cmd: str, cwd: TPPath = None, log: TPPath = None, pipe: bool = True) -> sp.CompletedProcess:
     """
     Execute the given command as a subprocess.
@@ -115,8 +46,8 @@ def execute(cmd: str, cwd: TPPath = None, log: TPPath = None, pipe: bool = True)
             s_out = StreamRedirect(name="stdout", stream_in=proc.stdout, stream_out=sys.stdout)
             # Wait for process and get data
             proc.wait()
-            s_err.wait_ready()
-            s_out.wait_ready()
+            s_err.join()
+            s_out.join()
             # Get buffers
             err = s_err.buffer
             out = s_out.buffer
@@ -133,7 +64,7 @@ def execute(cmd: str, cwd: TPPath = None, log: TPPath = None, pipe: bool = True)
     # Store logs (if required)
     if log is not None:
         with log.open(mode="w") as file:
-            file.write(f"Date: {time.strftime('%Y/%m/%d - %H:%M:%S', time.localtime())}"
+            file.write(f"Date: {time.strftime('%Y/%m/%d - %H:%M:%S', time.localtime())}\n"
                        f"CWD : {cwd}\n"
                        f"CMD : {cmd}\n"
                        f"RET : {res.returncode}\n"
