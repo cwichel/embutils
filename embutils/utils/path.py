d2f8179024d8425c4752e456324f5083b60c025c
Christian Wiche <cwichel@gmail.com>
2021-11-27 00:58:38 -0300

    Added file and directory validators

--- embutils/utils/path.py
+++ embutils/utils/path.py
@@ -52,13 +52,16 @@ def path_reachable(path: pl.Path) -> bool:
     return path.exists() or path.parent.exists()
 
 
-def path_validator(path: tp.Any, allow_none: bool = True, check_reachable: bool = False) -> tp.Optional[pl.Path]:
+def path_validator(path: tp.Any, allow_none: bool = True,
+                   check_existence: bool = False,
+                   check_reachable: bool = False) -> tp.Optional[pl.Path]:
     """
     Validate if the given parameter is a path.
 
     :param tp.Any path:             Path to be validated.
     :param bool allow_none:         Allow None inputs.
-    :param bool check_reachable:    Checks if the path is reachable.
+    :param bool check_existence:    Check if the path exists.
+    :param bool check_reachable:    Check if the path is reachable.
 
     :return: Path
     :rtype: pl.Path
@@ -73,5 +76,48 @@ def path_validator(path: tp.Any, allow_none: bool = True, check_reachable: bool 
     # Validate
     path = as_path(path=path)
     if check_reachable and not path_reachable(path=path):
-        raise ValueError(f"Path '{path}' is not reachable.")
+        raise ValueError(f"'{path}' is not reachable.")
+    if check_existence and not path.exists():
+        raise ValueError(f"'{path}' doesnt exist.")
+    return path
+
+
+def dir_validator(path: tp.Any, required: bool = False, create: bool = False):
+    """
+    Validate the directory path.
+
+    :param tp.Any path:     Directory path to be validated.
+    :param bool required:   Directory must exist.
+    :param bool create:     Directory can be created if reachable but not found. .
+
+    :return: Path
+    :rtype: pl.Path
+
+    :raises ValueError: Input invalid, directory not reachable or don't exist, path is not a directory.
+    """
+    path = path_validator(path=path, allow_none=False, check_reachable=True)
+    if create:
+        path.mkdir(exist_ok=True)
+    if path.exists() and not path.is_dir():
+        raise ValueError(f"'{path}' is not a directory.")
+    if required and not path.exists():
+        raise ValueError(f"'{path}' doesnt exist.")
+    return path
+
+
+def file_validator(path: tp.Any, required: bool = False):
+    """
+    Validate the file path.
+
+    :param tp.Any path:     File path to be validated.
+    :param bool required:   File must exist.
+
+    :return: Path
+    :rtype: pl.Path
+
+    :raises ValueError: Input invalid, file not reachable or don't exist, path is not a file.
+    """
+    path = path_validator(path=path, allow_none=False, check_reachable=True, check_existence=required)
+    if path.exists() and not path.is_file():
+        raise ValueError(f"Path '{path}' is not a file.")
     return path
